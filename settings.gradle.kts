8db1ebb5bdf18464fe8233bd1c55036441c05188
Muhammad Usama <lifeh2o@gmail.com>
2026-09-11 17:20:11 +0100

    Add settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "pixel-watch-heart-rate-alert"
+include(":app")
